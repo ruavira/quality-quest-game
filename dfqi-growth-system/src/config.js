edced5c764d64526e9f73ba6e0e2299c08b72f8c
ruavira <ruavirainc@gmail.com>
2026-08-11 11:58:39 -0600

    Add DFQI staging configuration

---
+++ dfqi-growth-system/src/config.js
@@ -0,0 +1,3 @@
+export const SUPABASE_URL = 'https://txhnnvwegshgkssqvcxu.supabase.co';
+export const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_YtI12gYcCIN1ADZVedY0vQ_Jh_phfwt';
+export const APP_VERSION = '1.0.0-staging';
